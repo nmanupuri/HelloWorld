b70b89cc348e69b9634890c46f83fa076f668342
nmanupuri <39561219+nmanupuri@users.noreply.github.com>
2019-04-26 07:28:48 +0530

    Add files via upload

---
+++ src/com/mnr/test/HelloWorld.java
@@ -0,0 +1,19 @@
+package com.mnr.test;
+
+public class HelloWorld {
+	
+	public static void main(String s[]) {
+		
+		System.out.println("Hello World");
+		
+		HelloWorld obj = new HelloWorld();
+		
+		System.out.println(obj.add(10, 20));
+		
+	}
+	
+	public int add(int a, int b) {
+		return a + b;
+	}
+
+}
